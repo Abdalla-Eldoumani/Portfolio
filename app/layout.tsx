15aac687dbd61e61d1e99b9d043a6491b5adf897
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-08-09 13:32:19 -0600

    Included the cursor component inside the root layout of the portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { CustomCursor } from "@/components/custom-cursor";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={font.className}>
+        <CustomCursor />
+        {children}
+      </body>
     </html>
   );
 }
